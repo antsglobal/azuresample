76bc054626d0385cb37e737a8a5e5f630f0466b0
Prashanth Devaraj <prashanth.devaraj@inveniodigital.com>
2021-12-01 09:14:31 +0530

    Testing with springboot container

--- src/main/java/net/codejava/Application.java
+++ src/main/java/net/codejava/Application.java
@@ -2,11 +2,20 @@ package net.codejava;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.boot.builder.SpringApplicationBuilder;  
+
 
 @SpringBootApplication
-public class Application {
+public class Application extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
 }
+
+@Override  
+protected SpringApplicationBuilder configure(SpringApplicationBuilder application)   
+{  
+return application.sources(SpringApplication.class);  
+}   
