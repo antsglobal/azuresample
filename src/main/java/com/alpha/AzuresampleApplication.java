7d64fa4a5b6a173168e98f303b41444d767e66e6
ramana <ramana.chinnapureddy@alpha-numero.com>
2021-12-01 12:32:48 +0530

    new change without db

--- src/main/java/com/alpha/AzuresampleApplication.java
+++ src/main/java/com/alpha/AzuresampleApplication.java
@@ -1,13 +1,26 @@
 package com.alpha;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+
 
 @SpringBootApplication
-public class AzuresampleApplication {
+
+public class AzuresampleApplication  extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AzuresampleApplication.class, args);
+	}
+	
+	@Override
+	  protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+	      return builder.sources(AzuresampleApplication.class);
 	}
 
 }
--- src/main/java/com/alpha/HomeController.java
+++ src/main/java/com/alpha/HomeController.java
@@ -9,23 +9,23 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.alpha.model.UserDetails;
-import com.alpha.model.UserLoginModel;
-import com.alpha.repository.UserRepository;
+//import com.alpha.model.UserDetails;
+//import com.alpha.model.UserLoginModel;
+//import com.alpha.repository.UserRepository;
 
 @RestController(value = "/")
 public class HomeController {
 	
-	@Autowired
-	UserRepository userRepository;
-	
+//	@Autowired
+//	UserRepository userRepository;
+//	
 	@Autowired
 	MyAppBean appBean;
 	
 	@GetMapping("/index")
 	public String index() {
 		//appBean.getMessage()
-	return "welcome to Index API"+appBean.getMessage()+"---"+appBean.getEnv();	
+	return "welcome to Index API";	
 	}
 	@GetMapping("/home")
 	public String home() {
@@ -37,9 +37,9 @@ public class HomeController {
 	return "This is Hello API";	
 	}
 	
-	@PostMapping("/login")
-	public Optional<UserDetails> userDetails(@RequestBody UserLoginModel userLoginModel) {
-		return userRepository.getUserDetailsBasedOnEmail(userLoginModel.getUserEmail());	
-	}
+//	@PostMapping("/login")
+//	public Optional<UserDetails> userDetails(@RequestBody UserLoginModel userLoginModel) {
+//		return userRepository.getUserDetailsBasedOnEmail(userLoginModel.getUserEmail());	
+//	}
 	
 	}
